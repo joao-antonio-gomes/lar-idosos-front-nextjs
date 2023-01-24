0e7b37216930ccbeed6d3adbb4e7339347c08b55
João Antônio Gomes <joao.antonio@softplan.com.br>
2023-01-24 08:29:19 -0300

    Alterado pagina de listagem de medicamentos para typescript

--- pages/remedio/index.tsx
+++ pages/remedio/index.tsx
@@ -1,20 +1,23 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import moment from 'moment';
 import Link from 'next/link';
 import { Button, Container, TextField } from '@mui/material';
 import { useSnackbar } from '../../src/context/snackbar';
-import { TableApp } from '../../src/components/tableApp';
 import ConfirmDialog from '../../src/components/confirmDialog';
 import MedicineService from '../../src/service/MedicineService';
 import Typography from '@mui/material/Typography';
+import Pageable from '../../src/interface/Pageable';
+import PageableFilter from '../../src/interface/PageableFilter';
+import TableColumn from '../../src/interface/TableColumn';
+import Medicine from '../../src/interface/Medicine';
+import TableApp from '../../src/components/tableApp';
 
-function RemediosListagem(props) {
+function RemediosListagem() {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [medicines, setMedicines] = useState();
+  const [medicines, setMedicines] = useState<Pageable>();
   const [openModalDeleteMedicine, setOpenModalDeleteMedicine] = useState(false);
-  const [medicineToDelete, setMedicineToDelete] = useState();
-  const [medicineFilter, setMedicineFilter] = useState({ page: 0, itemsPerPage: 10, name: null });
-  const columns = [
+  const [medicineToDelete, setMedicineToDelete] = useState<Medicine | undefined>();
+  const [medicineFilter, setMedicineFilter] = useState<PageableFilter>({ page: 0, size: 10, name: undefined, sort: 'id' });
+  const columns: TableColumn[] = [
     { field: 'id', headerName: 'ID', width: 70, sortable: false },
     { field: 'name', headerName: 'Nome', width: 160, sortable: false },
     {
@@ -32,13 +35,14 @@ function RemediosListagem(props) {
       sortable: false
     },
     {
+      field: 'actions',
       headerName: 'Ações',
       width: 120,
       sortable: false,
-      valueGetter: (patient) => {
+      valueGetter: (medicine: Medicine) => {
         return (
           <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-            <Link href={'/remedio/' + patient.id}>
+            <Link href={'/remedio/' + medicine.id}>
               <Button
                 variant="contained"
                 size={'small'}
@@ -51,7 +55,7 @@ function RemediosListagem(props) {
               variant="contained"
               size={'small'}
               style={{ fontWeight: 'bold' }}
-              onClick={() => handleOpenModalDeleteMedicine(patient)}
+              onClick={() => handleOpenModalDeleteMedicine(medicine)}
               color={'error'}>
               Excluir
             </Button>
@@ -62,13 +66,13 @@ function RemediosListagem(props) {
   ];
   const snackbar = useSnackbar();
 
-  const handleOpenModalDeleteMedicine = (patient) => {
+  const handleOpenModalDeleteMedicine = (medicine: Medicine) => {
     setOpenModalDeleteMedicine(true);
-    setMedicineToDelete(patient);
+    setMedicineToDelete(medicine);
   };
 
-  const handleDeleteMedicine = (patient) => {
-    MedicineService.delete(patient.id)
+  const handleDeleteMedicine = (medicine: Medicine | undefined) => {
+    MedicineService.delete(medicine?.id)
       .then((response) => {
         snackbar.showSnackBar('Remédio excluído com sucesso', 'success');
         fetchData();
@@ -87,23 +91,19 @@ function RemediosListagem(props) {
 
   const fetchData = useCallback(() => {
     MedicineService.getAll(medicineFilter).then(({ data }) => {
-      const patientsData = data.result.map((patient) => {
-        patient.age = moment().diff(patient.birthDate, 'years');
-        return patient;
-      });
-      setMedicines({ ...data, result: patientsData });
+      setMedicines(data);
     });
   }, [medicineFilter]);
 
-  let delayTimer;
-  const handleChangeSearchMedicine = (e) => {
+  let delayTimer: NodeJS.Timeout;
+  const handleChangeSearchMedicine = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     clearTimeout(delayTimer);
     delayTimer = setTimeout(function () {
-      setMedicineFilter({ ...medicineFilter, name: e.target.value });
+      setMedicineFilter({ ...medicineFilter, name: event.target.value });
     }, 700);
   };
 
-  const handlePageChange = (e, newPage) => {
+  const handlePageChange = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
     setMedicineFilter({ ...medicineFilter, page: newPage });
   };
 
@@ -134,7 +134,7 @@ function RemediosListagem(props) {
       </Link>
       <div style={{ margin: '10px 0 20px', display: 'flex', width: '100%' }}>
         <TextField
-          fullWidth={'100%'}
+          fullWidth={true}
           variant={'standard'}
           label={'Digite um nome para filtrar'}
           onChange={(e) => handleChangeSearchMedicine(e)}
@@ -142,17 +142,18 @@ function RemediosListagem(props) {
       </div>
       {isLoaded && (
         <TableApp
+          content={medicines?.content ?? [{}]}
+          count={medicines?.totalElements ?? 0}
+          page={medicines?.pageable.pageNumber ?? 0}
           columns={columns}
-          {...medicines}
           handlePageChange={handlePageChange}
-          noContentText="Não foi encontrado nenhum remédio."
-        />
+          noContentText='Não foi encontrado nenhum remédio.'        />
       )}
       {openModalDeleteMedicine && (
         <ConfirmDialog
           textButtonAgree={'Sim'}
           textButtonCancel={'Cancelar'}
-          dialogTitle={`Você deseja excluir o remédio ${medicineToDelete.name}?`}
+          dialogTitle={`Você deseja excluir o remédio ${medicineToDelete?.name}?`}
           dialogText={
             'Essa ação é irreversível e irá excluir o remédio cadastrado. O remédio só poderá ser excluído se não tiver sido utilizado em tratamentos.'
           }
---
+++ src/interface/Medicine.ts
@@ -0,0 +1,8 @@
+export default interface Medicine {
+  id: number;
+  name: string;
+  description: string;
+  concentration: number;
+  stock_quantity: number;
+  type: string;
+}
